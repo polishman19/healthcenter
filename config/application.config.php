f7570770ff7f0744a20b574949ce4d25a874bec3
szymzet <sz.zabkiewicz@gmail.com>
2013-11-10 12:28:49 +0100

    Remove unnecessary config and improve readability with new array syntax

--- config/application.config.php
+++ config/application.config.php
@@ -1,19 +1,19 @@
 <?php
-return array(
+return [
     // This should be an array of module namespaces used in the application.
-    'modules' => array(
+    'modules' => [
         'Application',
-    ),
+    ],
 
     // These are various options for the listeners attached to the ModuleManager
-    'module_listener_options' => array(
+    'module_listener_options' => [
         // This should be an array of paths in which modules reside.
         // If a string key is provided, the listener will consider that a module
         // namespace, the value of that key the specific path to that module's
         // Module class.
-        'module_paths' => array(
+        'module_paths' => [
             './module',
             './vendor',
-        ),
-    ),
-);
+        ],
+    ],
+];
