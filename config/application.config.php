d3630e649bb0c437ab802d3badb63f2f08103c20
szymzet <sz.zabkiewicz@gmail.com>
2013-11-10 12:16:42 +0100

    Initial commit with hello world zend framework 2 application

---
+++ config/application.config.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+    // This should be an array of module namespaces used in the application.
+    'modules' => array(
+        'Application',
+    ),
+
+    // These are various options for the listeners attached to the ModuleManager
+    'module_listener_options' => array(
+        // This should be an array of paths in which modules reside.
+        // If a string key is provided, the listener will consider that a module
+        // namespace, the value of that key the specific path to that module's
+        // Module class.
+        'module_paths' => array(
+            './module',
+            './vendor',
+        ),
+    ),
+);
